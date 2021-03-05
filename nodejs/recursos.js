70125907905554eea181556ab2d1d8ed2e501e49
Desarrollador Karen <karenvielma_rys@hotmail.com>
2021-03-04 19:26:10 -0600

    Ruta y handlers veterinarias, duenos, consultas

--- nodejs/recursos.js
+++ nodejs/recursos.js
@@ -6,4 +6,29 @@ module.exports = {
       { tipo: "perro", nombre: "Bella3", dueno: "Karen" },
       { tipo: "perro", nombre: "Bella4", dueno: "Karen" },
     ],
+
+    veterinarias: [
+      { nombre: "Karen", apellido: "Vielma", documento: "12345" },
+      { nombre: "Miguel", apellido: "Martinez", documento: "182900" },
+      { nombre: "Jose", apellido: "Gomez", documento: "88888" },
+      { nombre: "Fernanda", apellido: "Gonzalez", documento: "10000" },
+    ],
+
+    duenos: [
+      { nombre: "Maria", apellido: "Lucero", documento: "120000" },
+      { nombre: "Alondra", apellido: "Garza", documento: "9036478" },
+      { nombre: "Ricardo", apellido: "Luna", documento: "333388888" },
+      { nombre: "Natalia", apellido: "Salazar", documento: "567483" },
+    ],
+
+    consultas: [
+      {
+        mascota: 0,
+        veterinaria: 0,
+        fechaCreacion: new Date(),
+        fechaEdicion: new Date(),
+        historia: "",
+        diagnostico: "",
+      },
+    ],
   };
