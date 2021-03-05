d8d4c94724d9d585c7e18248faf9b3eab388baf2
Desarrollador Karen <karenvielma_rys@hotmail.com>
2021-03-04 18:29:17 -0600

    Metodo DELETE y ordenamiento de handlers de rutas

--- nodejs/enrutador.js
+++ nodejs/enrutador.js
@@ -1,23 +1,11 @@
+const recursos = require("./recursos");
+const mascotas = require("./rutas/mascotas");
+
 module.exports = { 
     ruta: (data, callback) => {
       callback(200, {mensaje: "Estas es /ruta"});
     },
-    mascotas: {
-      get: (data, callback) => {
-        if (typeof data.indice !== "undefined") {
-          console.log("handler mascotas", { data });
-          if (global.recursos.mascotas[data.indice]) {
-            return callback(200, global.recursos.mascotas[data.indice]);
-          }
-          return callback(404, {mensaje: "Mascota con indice ${data.indice} no encontrada",});
-        }
-        callback(200, global.recursos.mascotas);
-      },
-      post: (data, callback) => {
-        global.recursos.mascotas.push(data.payload);
-        callback(201, data.payload);
-      },
-    },
+    mascotas: mascotas(recursos.mascotas),
     noEncontrado: (data, callback) => {
       callback(404, {mensaje: "Ruta No encontrada"});
     },
--- nodejs/recursos.js
+++ nodejs/recursos.js
@@ -1,9 +1,9 @@
 module.exports = {
     mascotas: [
-      { tipo: "perro", nombre: "Bella", dueno: "Karen" },
-      { tipo: "perro", nombre: "Bella", dueno: "Karen" },
-      { tipo: "perro", nombre: "Bella", dueno: "Karen" },
-      { tipo: "perro", nombre: "Bella", dueno: "Karen" },
-      { tipo: "perro", nombre: "Bella", dueno: "Karen" },
+      { tipo: "perro", nombre: "Bella0", dueno: "Karen" },
+      { tipo: "perro", nombre: "Bella1", dueno: "Karen" },
+      { tipo: "perro", nombre: "Bella2", dueno: "Karen" },
+      { tipo: "perro", nombre: "Bella3", dueno: "Karen" },
+      { tipo: "perro", nombre: "Bella4", dueno: "Karen" },
     ],
   };
