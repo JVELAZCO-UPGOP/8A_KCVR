fcd6fe3e2b001a80cf2d0b25d63b403fb9bfbbf9
Desarrollador Karen <karenvielma_rys@hotmail.com>
2021-04-25 01:51:21 -0500

    filtrado de mascotas

---
+++ node-react/backend/rutas/mascotas.js
@@ -0,0 +1,64 @@
+module.exports = function mascotasHandler(mascotas){
+    return   {
+    get: (data, callback) => {
+      console.log("handler mascotas", { data });
+      if (typeof data.indice !== "undefined") {
+       
+        if (mascotas[data.indice]) {
+          return callback(200, mascotas[data.indice]);
+        }
+        return callback(404, {mensaje: "La Mascota con indice ${data.indice} no encontrada",});
+      }
+      if (
+        data.query &&
+        (typeof data.query.nombre !== "undefined" ||
+          data.query.tipo !== "undefined" ||
+          data.query.dueno !== "undefined")
+      ) {
+        const llavesQuery = Object.keys(data.query);
+        let respuestaMascotas = [...mascotas];
+        for (const llave of llavesQuery) {
+          respuestaMascotas = respuestaMascotas.filter((_mascota) => {
+            const expresionRegular = new RegExp(data.query[llave], "ig");
+            const resultado = _mascota[llave].match(expresionRegular);
+            return resultado;
+          });
+        }
+        return callback(200, respuestaMascotas);
+      }
+      callback(200, mascotas);
+    },
+    post: (data, callback) => {
+      mascotas.push(data.payload);
+      callback(201, data.payload);
+    },
+    put: (data, callback) => {
+      if (typeof data.indice !== "undefined") {
+        if (mascotas[data.indice]) {
+          mascotas[data.indice] = data.payload;
+          return callback(200, mascotas[data.indice]);
+        }
+        return callback(404, {
+          mensaje: `La maascota con indice ${data.indice} no encontrada`,
+        });
+      }
+      callback(400, { mensaje: "Indice no enviado" });
+    },
+    delete: (data, callback) => {
+      if (typeof data.indice !== "undefined") {
+        if (mascotas[data.indice]) {
+          mascotas = mascotas.filter(
+            (_mascota, indice) => indice != data.indice
+          );
+          return callback(204, {
+            mensaje: "El elemento con indice ${data.indice} eliminado",
+          });
+        }
+        return callback(404, {
+          mensaje: "La mascota con indice ${data.indice} no encontrada",
+        });
+      }
+      callback(400, { mensaje: "Indice no enviado" });
+    },
+  };
+};
